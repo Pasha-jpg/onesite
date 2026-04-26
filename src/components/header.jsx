9c788a2c108d919d6a089b7411334bde016232e2
pasha <70097930+Pasha-jpg@users.noreply.github.com>
2026-04-26 16:03:04 +0800

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -21,7 +21,7 @@ function Header(){
         </div>
      <div className="A">
         <h1>
-            <img src={`${process.env.PUBLIC_URL}/images/logo192.png`} alt="Logo" />
+            <img src={`${process.env.PUBLIC_URL}/logo192.png`} alt="Logo" />
         </h1>
         
         <nav className="Navbar">
