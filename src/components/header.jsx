7e669ab65c2890d8df147994950c57383bf0a645
pasha <70097930+Pasha-jpg@users.noreply.github.com>
2026-04-26 15:57:43 +0800

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -21,7 +21,7 @@ function Header(){
         </div>
      <div className="A">
         <h1>
-            <img src={`${process.env.PUBLIC_URL}/images/logo192.png`} alt="ONE RING RENTALS Logo" />
+            <img src={`${process.env.PUBLIC_URL}/images/logo192.png`} alt="Logo" />
         </h1>
         
         <nav className="Navbar">
@@ -44,5 +44,3 @@ function Header(){
 }
 
 export default Header;
-
-
